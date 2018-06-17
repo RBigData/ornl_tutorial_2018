f980b3b97a7519a8ef837f15e1ff7122a5fa0e1f
wrathematics <wrathematics@gmail.com>
2018-06-17 14:57:09 -0400

    add dockerfiles

---
+++ docker/shiny/shinykmeans/ui.r
@@ -0,0 +1,16 @@
+library(shiny)
+
+ui <- pageWithSidebar(
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
+
